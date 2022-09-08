0bcd316740a9e639544a48b1a84cb16aa38e7615
Martin Jäger <martin@libre.solar>
2022-09-08 16:20:50 +0200

    Update Pandoc and make CI fail on error in build.sh

--- spec/build.sh
+++ spec/build.sh
@@ -3,12 +3,15 @@
 # This script helps to build the document using the pandoc/ubuntu-latex Docker image in
 # a CI/CD environment. See also .github/workflows/pandoc.yml
 
+# exit when any command fails
+set -e
+
 # Add potentially missing LaTeX packages for Eisvogel template
 tlmgr update --self
 tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook \
-        footmisc footnotebackref framed fvextra letltxmacro ly1 mdframed mweights needspace \
-        pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math upquote xecjk \
-        xurl zref
+        footmisc footnotebackref framed fvextra koma-script letltxmacro ly1 mdframed mweights \
+        needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math \
+        upquote xecjk xurl zref
 
 # Install GNU make
 apt-get update
