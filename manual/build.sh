72007482dec4ebd48a6f8401208dc8f2e4de56a8
Martin Jäger <martin@libre.solar>
2023-01-20 08:53:16 +0100

    Fix Pandoc deployment

--- manual/build.sh
+++ manual/build.sh
@@ -8,10 +8,38 @@ set -e
 
 # Add potentially missing LaTeX packages for Eisvogel template
 tlmgr update --self
-tlmgr install adjustbox babel-german background bidi collectbox csquotes everypage filehook \
-        footmisc footnotebackref framed fvextra koma-script letltxmacro ly1 mdframed mweights \
-        needspace pagecolor sourcecodepro sourcesanspro titling ucharcat ulem unicode-math \
-        upquote xecjk xurl zref
+tlmgr install \
+        adjustbox \
+        babel-german \
+        background \
+        bidi \
+        catchfile \
+        collectbox \
+        csquotes \
+        everypage \
+        filehook \
+        footmisc \
+        footnotebackref \
+        framed \
+        fvextra \
+        hardwrap \
+        koma-script \
+        letltxmacro \
+        ly1 \
+        mdframed \
+        mweights \
+        needspace \
+        pagecolor \
+        sourcecodepro \
+        sourcesanspro \
+        titling \
+        ucharcat \
+        ulem \
+        unicode-math \
+        upquote \
+        xecjk \
+        xurl \
+        zref
 
 # Install GNU make
 apt-get update
